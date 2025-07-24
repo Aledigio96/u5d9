fea230ca65994f633986df3cf9887f771e0ea23a
Aledigio96 <alessandro.digiovanni10@gmail.com>
2025-07-24 15:44:42 +0200

    second commit
    fixato errore in env.properties e varie validation iniziali

--- src/main/java/digiovannialessandro/u5d9/entities/Author.java
+++ src/main/java/digiovannialessandro/u5d9/entities/Author.java
@@ -3,6 +3,10 @@ package digiovannialessandro.u5d9.entities;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,15 +24,19 @@ public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotEmpty(message = "Il nome è obbligatorio")
+    @Size(min = 2, max = 40, message = "Il nome deve essere di lunghezza compresa tra 2 e 40")
     private String name;
+    @NotEmpty(message = "Il cognome è obbligatorio")
+    @Size(min = 2, max = 40, message = "Il nome deve essere di lunghezza compresa tra 2 e 40")
     private String surname;
+    @NotEmpty(message = "L'indirizzo email è obbligatorio")
+    @Email(message = "L'indirizzo email inserito non è nel formato giusto")
     private String email;
+    @NotEmpty(message = "La data di nascita è obbligatoria")
+    @Past(message = "La data deve essere in un giorno|anno passato")
     private String dateOfBirth;
     private String avatar;
 
-/* Per evitare lo stackoverflow error che avviene quando si manda un Author come risposta (può avvenire anche quando mandiamo un BlogPost con un Author)
-   si può o togliere la bidirezionalità, oppure usare @JsonIgnore, oppure farsi un payload di risposta custom.
-  @OneToMany(mappedBy = "author", cascade = CascadeType.REMOVE)
-    @JsonIgnore
-    List<Blogpost> blogpostList;*/
+
 }
--- src/main/java/digiovannialessandro/u5d9/payloads/NewBlogPostPayload.java
+++ src/main/java/digiovannialessandro/u5d9/payloads/NewBlogPostPayload.java
@@ -1,12 +1,22 @@
 package digiovannialessandro.u5d9.payloads;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class NewBlogPostPayload {
     private int authorId;
+    @NotEmpty
+    @Size(min = 2,max = 15,message = "La categoria deve essere compresa tra2 e 15 caratteri")
     private String category;
+    @NotEmpty
+    @Size(min = 10,max = 200,message = "Il contenuto deve essere di almeno 10 caratteri e non deve superari i 200")
     private String content;
+    @Positive
     private double readingTime;
+    @NotEmpty
+    @Size(min = 2,max = 30,message = "Il titolo deve essere compreso tra i 2 e i 30 caratteri")
     private String title;
 }
